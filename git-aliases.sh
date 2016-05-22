2f5ce77823643b60ee6ae6a83ddd234e089a0907
Jabez Wilson <jabez.wilson0@gmail.com>
2016-05-23 09:14:13 +1000

    added first file

---
+++ git-aliases.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+git config --global alias.ac '!git add -A && git commit'
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.co checkout
+git config --global alias.sb 'status -sb'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.rst 'reset --hard HEAD'
