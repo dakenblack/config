4f9c2f7941a8ac60dce3bc9b5e53d605e689b1f3
Jabez Wilson <jabez.wilson0@gmail.com>
2018-02-16 16:50:55 +1100

    added comments and the nerdcommenter plugin

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
           curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# vimrc
 cp vim.vimrc ~/.vimrc
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
